34ef00f4bc57923b12ea691d774a7dc649479571
Maxim Umblia <umblia34@gmail.com>
2024-12-12 17:59:09 +0300

    equip factory update

--- database/factories/EquipmentFactory.php
+++ database/factories/EquipmentFactory.php
@@ -24,17 +24,25 @@ class EquipmentFactory extends Factory
 
         return [
             'manufactor' => $this->faker->company,
-            'category_id' => \App\Models\EquipmentCategories::first()->id ?? null,  // Select the first category
-            'size_id' => \App\Models\EquipmentSize::first()->id ?? null,  // Select the first size
-            'series' => $this->faker->bothify('ДР-####'),
-            'location_id' => \App\Models\EquipmentLocation::inRandomOrder()->first()->id ?? null,
-            'length' => $this->faker->numberBetween(20, 100),
-            'operating' => $this->faker->numberBetween(20, 100),
-            'manufactor_date' => $this->faker->date(),
-            'status' => $this->faker->randomElement(['new', 'good', 'satisfactory', 'bad', 'off']),
-            'notes' => $this->faker->sentence(),
-            'price' => $this->faker->numberBetween(4000, 50000),
-            'commentary' => $this->faker->paragraph(),
+            'category_id' => \App\Models\EquipmentCategories::inRandomOrder()->first()->id?? null,
+           'size_id' => \App\Models\EquipmentSize::inRandomOrder()->first()->id?? null,
+            'location_id' => \App\Models\EquipmentLocation::inRandomOrder()->first()->id?? null,
+           'series' => $this->faker->bothify('ДР-####'),
+            'length' => $this->faker->optional()->numberBetween(20, 100),
+            'operating' => $this->faker->optional()->numberBetween(20, 100),
+           'manufactor_date' => $this->faker->optional()->date(),
+           'status' => $this->faker->optional()->randomElement(['new', 'good','satisfactory', 'bad', 'off']),
+            'notes' => $this->faker->optional()->sentence(),
+            'price' => $this->faker->optional()->numberBetween(4000, 50000),
+            'commentary' => $this->faker->optional()->paragraph(),
+            'zahodnost' => $this->faker->optional()->word,
+           'stator_rotor' => $this->faker->optional()->word,
+            'dlina_ds' => $this->faker->optional()->word,
+            'narabotka_ds' => $this->faker->optional()->word,
+           'rezbi' => $this->faker->optional()->word,
+            'length_rezba' => $this->faker->optional()->word,
+            'diameter' => $this->faker->optional()->word,
+            'hyperlink' => $this->faker->optional()->url,
         ];
     }
 }
