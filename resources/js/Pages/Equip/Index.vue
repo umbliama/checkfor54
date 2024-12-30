4a45c0f3957c507af0325c98608367407c2afc9c
Harut <metsatunyanharut7@gmail.com>
2024-12-30 19:20:56 +0300

    Equip fix

--- resources/js/Pages/Equip/Index.vue
+++ resources/js/Pages/Equip/Index.vue
@@ -472,7 +472,9 @@ onMounted(() => {
                                                         </Link>
                                                     </DropdownMenuItem>
                                                     <DropdownMenuItem>
-                                                        <Link :href="route('equip.destroy', item.id)"
+                                                        <Link
+                                                            :href="route('equip.destroy', item.id)"
+                                                            method="DELETE"
                                                             class="inline-flex items-center py-1 px-2 rounded text-danger hover:bg-my-gray transition-all">
                                                         Удалить
                                                         <svg class="block ml-2" width="16" height="16"
@@ -630,7 +632,9 @@ onMounted(() => {
                                                         </Link>
                                                     </DropdownMenuItem>
                                                     <DropdownMenuItem>
-                                                        <Link :href="route('equip.destroy', item.id)"
+                                                        <Link
+                                                            :href="route('equip.destroy', item.id)"
+                                                            method="DELETE"
                                                             class="inline-flex items-center py-1 px-2 rounded text-danger hover:bg-my-gray transition-all">
                                                         Удалить
                                                         <svg class="block ml-2" width="16" height="16"
