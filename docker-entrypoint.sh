d859f3a3b6c8f770fdd35293f9216a881ae5e8e2
Maxim Umblia <umblia34@gmail.com>
2025-02-24 17:27:40 +0300

    fix docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 # Wait for database (MySQL/Postgres)
 echo "Waiting for database connection..."
 sleep 5 # Adjust this if needed
+ls -la /var/www/
+
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 # Run Laravel commands
 php artisan migrate --force
