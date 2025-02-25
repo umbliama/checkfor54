91e922e44da0edf256c43c1f6946b7899a8c5cb5
Maxim Umblia <umblia34@gmail.com>
2025-02-26 00:29:52 +0300

    fix

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,7 +5,7 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: process.env.MIX_REVERB_APP_KEY,
+    key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: 'terra-crm.ru',
     wsPort: 443,           // or leave it out if using wss on 443
     wssPort: 443,          // ensure TLS is used on port 443
