f806ab97cb64c75ce1becaa8efa04adf229bcebc
Maxim Umblia <umblia34@gmail.com>
2025-02-26 00:16:31 +0300

    fix

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,10 +5,10 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    key: process.env.MIX_REVERB_APP_KEY,
+    wsHost: 'terra-crm.ru',
+    wsPort: 443,           // or leave it out if using wss on 443
+    wssPort: 443,          // ensure TLS is used on port 443
+    forceTLS: true,
+    disableStats: true,
 });
