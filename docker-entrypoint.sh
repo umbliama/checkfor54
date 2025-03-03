f9e7148de8fb1839302ae97c0b5aa8a4e2cc8ac1
Maxim Umblia <umblia34@gmail.com>
2025-03-04 00:11:04 +0300

    entry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,6 @@ chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 # Start services
-php artisan migrate --force
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
