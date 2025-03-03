f99ecb83ca35cda8e8aaa83f6c9ca077772cb93a
Maxim Umblia <umblia34@gmail.com>
2025-03-03 23:58:23 +0300

    mig

--- database/migrations/2025_03_03_205759_add_ownership_to_equipment_table.php
+++ database/migrations/2025_03_03_205759_add_ownership_to_equipment_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('equipment', function (Blueprint $table) {
             $table->enum('ownership', ['our', 'sub'])->nullable()->after('hyperlink');
+
         });
     }
 
