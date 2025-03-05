854530927b32729ab19be7650999a71610b19568
Maxim Umblia <umblia34@gmail.com>
2025-03-05 21:58:56 +0300

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 set -e
 
+if [ ! -f /var/www/.env ]; then
+  cat <<EOF > /var/www/.env
+APP_ENV=production
+APP_DEBUG=true
+APP_KEY=${APP_KEY}
+BROADCAST_DRIVER=${BROADCAST_DRIVER}
+REVERB_APP_ID=${REVERB_APP_ID}
+REVERB_APP_KEY=${REVERB_APP_KEY}
+REVERB_APP_SECRET=${REVERB_APP_SECRET}
+REVERB_HOST=${REVERB_HOST}
+REVERB_PORT=${REVERB_PORT}
+REVERB_SCHEME=${REVERB_SCHEME}
+EOF
+fi
+
+exec "$@"
+
 echo "Waiting for database connection..."
 sleep 5 # Adjust as needed
 
