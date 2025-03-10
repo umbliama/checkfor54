a5c3a73603716758e9e95e0dc607d97c8325fda1
Maxim Umblia <umblia34@gmail.com>
2025-03-10 22:40:15 +0300

    chat

---
+++ routes/chatify/web.php
@@ -0,0 +1,106 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\vendor\Chatify\MessagesController;
+use App\Http\Controllers\ChatGroup;
+
+/*
+* Main Chatify Route
+*/
+Route::get('/', [MessagesController::class, 'index'])->name(config('chatify.routes.prefix'));
+
+/**
+ *  Fetch info for specific id [user/group]
+ */
+Route::post('/idInfo', [MessagesController::class, 'idFetchData']);
+
+Route::post('//groupidInfo', [ChatGroup::class, 'idFetchData']);
+
+/**
+ * Send message route
+ */
+Route::post('/sendMessage', [MessagesController::class, 'send'])->name('send.message');
+Route::post('/sendMessageGroup', [ChatGroup::class, 'send'])->name('send.groupMessage');
+
+/**
+ * Fetch messages
+ */
+Route::post('/fetchMessages', [MessagesController::class, 'fetch'])->name('fetch.messages');
+
+/**
+ * Download attachments route
+ */
+Route::get('/download/{fileName}', [MessagesController::class, 'download'])->name(config('chatify.attachments.download_route_name'));
+
+/**
+ * Authentication for pusher private channels
+ */
+Route::post('/chat/auth', [MessagesController::class, 'pusherAuth'])->name('pusher.auth');
+
+/**
+ * Make messages as seen
+ */
+Route::post('/makeSeen', [MessagesController::class, 'seen'])->name('messages.seen');
+
+/**
+ * Get contacts
+ */
+Route::get('/getContacts', [MessagesController::class, 'getContacts'])->name('contacts.get');
+
+/**
+ * Update contact item data
+ */
+Route::post('/updateContacts', [MessagesController::class, 'updateContactItem'])->name('contacts.update');
+
+/**
+ * Star in favorite list
+ */
+Route::post('/star', [MessagesController::class, 'favorite'])->name('star');
+
+/**
+ * Get favorites list
+ */
+Route::post('/favorites', [MessagesController::class, 'getFavorites'])->name('favorites');
+
+/**
+ * Search in messenger
+ */
+Route::get('/search', [MessagesController::class, 'search'])->name('search');
+
+/**
+ * Get shared photos
+ */
+Route::post('/shared', [MessagesController::class, 'sharedPhotos'])->name('shared');
+
+/**
+ * Delete conversation
+ */
+Route::post('/deleteConversation', [MessagesController::class, 'deleteConversation'])->name('conversation.delete');
+
+/**
+ * Delete message
+ */
+Route::post('/deleteMessage', [MessagesController::class, 'deleteMessage'])->name('message.delete');
+
+/**
+ * Update settings
+ */
+Route::post('/updateSettings', [MessagesController::class, 'updateSettings'])->name('avatar.update');
+
+/**
+ * Set active status
+ */
+Route::post('/setActiveStatus', [MessagesController::class, 'setActiveStatus'])->name('activeStatus.set');
+
+/**
+ * Group view by id
+ */
+Route::get('/group/{id}', [MessagesController::class, 'groupIndex'])->name('group');
+
+/**
+ * User view by id.
+ * Note: Dynamic routes must always be at the end.
+ */
+Route::get('/{id}', [MessagesController::class, 'index'])->name('user');
+
+
